10b4cd8e8f22c65cbfb0789978e67a664eae646f
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-10-10 12:41:39 +0700

    belajar

---
+++ src/TipeDataChar.java
@@ -0,0 +1,14 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+
+        char e = 'E';
+        char k = 'K';
+        char o = 'O';
+
+//        System.out.print(e); ini tidak ada enter
+        System.out.println(e);
+        System.out.println(k);
+        System.out.println(o);
+
+    }
+}
--- src/TipeDataNumber.java
+++ src/TipeDataNumber.java
@@ -7,7 +7,14 @@ public class TipeDataNumber {
         long iniLong2 = 1000000000L;
 
         float iniFloat = 10.10F;
-        double
+        double iniDouble = 10.10;
+
+        int decimalLInt = 25;
+        int hexInt = 0xA132B;
+        int binInt = 0b01010101;
+
+        long balance = 1_000_000_000_000L;
+        int sum = 60_000_000;
 
     }
 }
