c7971a24ed3fe0c2dbc169a65dbb30c4a8df2523
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-10-15 18:16:05 +0700

    update

---
+++ src/SwitchStatement.java
@@ -0,0 +1,34 @@
+public class SwitchStatement {
+    public static void main(String[] args) {
+        var nilai = "A";
+
+        switch (nilai) {
+            case "A":
+                System.out.println("Wow, Anda Lulus Dengan Baik");
+                break;
+            case "B":
+            case "C":
+                System.out.println("Nilai Anda Cukup Baik");
+                break;
+            case "D":
+                System.out.println("Anda Tidak Lulus");
+                break;
+            default:
+                System.out.println("Mungkin Anda Salah Jurusan");
+        }
+
+
+        switch (nilai) {
+            case "A" -> System.out.println("Wow, Anda Lulus Dengan Baik");
+            case "B" , "C" -> System.out.println("Nilai Anda Cukup Baik");
+            case "D" -> System.out.println("anda belum lulus");
+
+        }
+
+
+
+
+
+    }
+
+}
